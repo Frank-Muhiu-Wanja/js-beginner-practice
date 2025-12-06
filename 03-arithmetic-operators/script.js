8819f85c5095a42184112a9187c492420efa87d4
Frank-Muhiu-Wanja <frankmuhiu648@gmail.com>
2025-12-07 02:03:05 +0300

    Added arithmetic practice folder

---
+++ 03-arithmetic-operators/script.js
@@ -0,0 +1,34 @@
+// arithmetic operators = operands (values, variales, etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+let students = 30;
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 3; power of
+// let extrastudents = students % 7; modulous
+
+// students += 2;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+/*
+   operator precedence
+    1. paranthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+    
+*/
+
+let result = 23;
+
+console.log(result);
