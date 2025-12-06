6430a341415dc8cef7f39810bfdf81c349cde843
Frank-Muhiu-Wanja <frankmuhiu648@gmail.com>
2025-12-07 01:54:07 +0300

    Added 01-beginner

---
+++ 01-beginner/script.js
@@ -0,0 +1,13 @@
+// console.log(`hello`);
+// console.log(`I like pizza!`);
+// window.alert(`this is an alert`);
+// window.alert(`I like pizza!`);
+
+// This is a comment
+
+/*
+This is a comment
+*/
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`;
---
+++ 02-variables/script.js
@@ -0,0 +1,32 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the value it contains.
+
+// 1. declaration   let x;
+// 2. assignment    x= 100;
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+// let firstName = `Frank`;
+// let favoriteFood = "sushi";
+// let email = "frank666@gmail.com";
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+let fullName = "Frank Nice";
+let age = 24;
+let student = false;
+
+document.getElementById("p1").textContent = `My Name is ${fullName}`;
+document.getElementById("p2").textContent = `I am ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+// console.log(`Bro is online: ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Enrolled: ${isStudent}`);
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You like ${favoriteFood}`);
+// console.log(`Your email is ${email}`);
+// console.log(`You are ${age} years old`);
+// console.log(`The price is ${price}`);
+// console.log(`Your gpa is: ${gpa}`);
