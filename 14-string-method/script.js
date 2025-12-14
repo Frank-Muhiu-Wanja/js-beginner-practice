d046d64c7fbd469f2b6afc89cf652d1b7bbe3cb8
Frank-Muhiu-Wanja <frankmuhiu648@gmail.com>
2025-12-14 21:22:15 +0300

    Added folder 14

---
+++ 14-string-method/script.js
@@ -0,0 +1,86 @@
+// String methods = allow you to manipulate and work with text (strings)
+
+/*
+let userName = "Brocode    ";
+
+console.log(userName.charAt(3));
+console.log(userName.indexOf("e"));
+console.log(userName.lastIndexOf("o"));
+console.log(userName.length);
+*/
+
+/*
+let userName = "Brocode    ";
+userName = userName.trim();
+console.log(userName);
+*/
+
+/*
+let userName = "Brocode";
+userName = userName.toUpperCase();
+console.log(userName);
+*/
+
+/*
+let userName = "BROCODE    ";
+userName = userName.toLowerCase();
+console.log(userName);
+*/
+
+/*
+let userName = "Brocode";
+userName = userName.repeat(3);
+console.log(userName);
+*/
+
+/*
+let userName = " Brocode";
+let result = userName.startsWith(" ");
+
+if (result) {
+  console.log("Your username can't begin with ' '");
+} else {
+  console.log(userName);
+}
+*/
+
+/*
+let userName = "Brocode    ";
+let result = userName.endsWith(" ");
+
+if (result) {
+  console.log("Your username can't end with ' '");
+} else {
+  console.log(userName);
+}
+*/
+
+/*
+let userName = "Brocode    ";
+let result = userName.includes(" ");
+
+if (result) {
+  console.log("Your username can't include ' '");
+} else {
+  console.log(userName);
+}
+*/
+
+/*
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.replaceAll("-", "/");
+console.log(phoneNumber);
+*/
+
+/*
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.padStart(50, "0");
+console.log(phoneNumber);
+*/
+
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.padEnd(50, "0");
+console.log(phoneNumber);
