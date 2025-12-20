61b1741ae9b4a48eb13dc6f6920dbcdb1a118707
Frank-Muhiu-Wanja <frankmuhiu648@gmail.com>
2025-12-20 04:37:17 +0300

    added folder 23

---
+++ 23-variable-scope/script.js
@@ -0,0 +1,34 @@
+// variable scope = where a variable is recognized
+//       and accessible (local vs global)
+/*
+Local Scope
+
+function1();
+function2();
+
+function function1() {
+  let x = 1;
+  console.log(y);
+}
+function function2() {
+  let y = 2;
+  console.log(x);
+}
+*/
+
+//Global scope
+
+let x = 3;
+
+function1();
+function2();
+
+function function1() {
+  let x = 1;
+  console.log(x);
+}
+
+function function2() {
+  let x = 2;
+  console.log(x);
+}
